9f9c5dc435289d10d7617b3ed8ae997b2c4b5841
Mattias.Poppe <poppe.mattias@gmail.com>
2025-07-18 15:58:13 +0200

    Fix tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,14 @@
-import pytest
 from meemoo_sip_validator.sip_validator import MeemooSIPValidator
 from pathlib import Path
 
 
 def test_sanity_check():
     assert True
-    
-    
+
+
 def test_validator_library():
-    validator = MeemooSIPValidator()
-    succes, report = validator.validate(Path("./tests/resources/film_sip"))
+    validator = MeemooSIPValidator(Path("./tests/resources/film_sip"))
+    is_valid = validator.validate()
     # At the moment the example sip is not EARK compliant.
-    assert not succes
-    assert report
+    assert not is_valid
+    assert validator.validation_report.to_dict() # Simple check
